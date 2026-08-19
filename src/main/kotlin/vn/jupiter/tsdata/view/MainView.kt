3ccbe5dc228e8a7c87888efae2ce7daf4865e84a
qbnguyen0604-web <qbnguyen0604@gmail.com>
2026-08-19 23:33:09 +0700

    Update MainView.kt

--- src/main/kotlin/vn/jupiter/tsdata/view/MainView.kt
+++ src/main/kotlin/vn/jupiter/tsdata/view/MainView.kt
@@ -16,6 +16,7 @@ class MainView : View("TS Data Editor - Ultimate Version") {
             "Talk" to ItemTabView(ItemTabController(TalkDataRepo())),
             "Skill" to ItemTabView(ItemTabController(SkillDataRepo())),
             "Scene" to ItemTabView(ItemTabController(SceneSkillDataRepo()))
+            "Shop (file8.dat)" to ItemTabView(ItemTabController(ShopDataRepo(), ShopDataRepo()))
     )
 
     override val root = borderpane {
